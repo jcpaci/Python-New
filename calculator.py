2b4560dc4f9b9b721e80760d9962dcad89fade0a
James Paci <jcpaci@Jamess-MacBook-Air.local>
2017-01-11 22:08:47 -0500

    made this document way more impressive

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def multiply(a,b):
+	print "I'm multiplying two numbers"
+	return a*b
+
+def add(a,b):
+	print "I'm adding two numbers"
+	return a + b
+
+def subtract(a,b):
+	print "I'm subtracting two numbers"
+	return a - b
+
+def divide(a,b):
+	print "I'm dividing b by a"
+	return a / b
+
+print "I'm going to use the calculator functions to multiply, add, subtract, and divdide 5 and 6"
+x = multiply (5,6)
+print x
+y = add(5,6)
+print y
+z = subtract(5,6)
+print z
+w = divide(5,6)
+print w 
