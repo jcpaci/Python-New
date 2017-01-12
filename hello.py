05eeae0bc6e03a67f177c6b239d53594cbef14ec
James Paci <jcpaci@Jamess-MacBook-Air.local>
2017-01-11 21:18:47 -0500

    did some math

---
+++ hello.py
@@ -0,0 +1,17 @@
+def multiply(a,b)
+	print "I'm multiplying two numbers"
+	return a * b
+
+def add(a,b)
+	print "I'm adding two numbers"
+	return a + b
+
+def subtract(a,b)
+	print "I'm subtracting two numbers"
+	return a - b
+
+def divide(a,b)
+	print "I'm dividing b by a"
+	return a / b
+
+print "I'm going to use the calculator functions to multiply 5 and 6"
